70e32c43eeed7208d2ff619ff41c921945641cb8
Roberto Hurtado Jr <rhurtado.267@gmail.com>
2023-10-31 19:03:13 -0500

    voteRoutes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+// Tell router object to use routes
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
 
 module.exports = router;
